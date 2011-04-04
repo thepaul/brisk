ab73a99cea0a45ab11cd29d1157e3b02285ee6d9
T Jake Luciani <jakers@gmail.com>
2011-04-04 12:36:48 -0500

    Added test suite and CFS test

---
+++ test/unit/org/apache/cassandra/EmbeddedServer.java
@@ -0,0 +1,67 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.cassandra;
+
+import java.io.IOException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+import com.datastax.brisk.BriskDaemon;
+
+
+public class EmbeddedServer 
+{
+    protected static BriskDaemon daemon = null;
+       
+    static ExecutorService executor = Executors.newSingleThreadExecutor();
+    
+    public static void startBrisk() throws IOException
+
+    {
+        executor.execute(new Runnable()
+        {
+            public void run()
+            {
+                daemon = new com.datastax.brisk.BriskDaemon();
+               
+                daemon.activate();
+            }
+        });
+        try
+        {
+            TimeUnit.SECONDS.sleep(3);
+        }
+        catch (InterruptedException e)
+        {
+            throw new AssertionError(e);
+        }
+    }
+    
+    public static void stopBrisk() throws Exception
+    {
+        if (daemon != null)
+        {
+            daemon.deactivate();
+        }
+        executor.shutdown();
+        executor.shutdownNow();
+    }
+    
+}
