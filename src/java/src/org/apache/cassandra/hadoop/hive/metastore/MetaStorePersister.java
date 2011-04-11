3c0ad44e2d922afa10c8ee108bdcdfd9eb489ca5
zznate <nate@vervewireless.com>
2011-04-11 14:07:02 -0500

    first cut of generic persistence and loading of model objects for metastore, coverage for such

--- src/java/src/org/apache/cassandra/hadoop/hive/metastore/MetaStorePersister.java
+++ src/java/src/org/apache/cassandra/hadoop/hive/metastore/MetaStorePersister.java
@@ -1,9 +1,20 @@
 package org.apache.cassandra.hadoop.hive.metastore;
 
+import java.nio.ByteBuffer;
+import java.util.Arrays;
 import java.util.Map;
 
+import org.apache.cassandra.thrift.Cassandra;
+import org.apache.cassandra.thrift.Column;
+import org.apache.cassandra.thrift.ColumnOrSuperColumn;
+import org.apache.cassandra.thrift.ColumnPath;
+import org.apache.cassandra.thrift.ConsistencyLevel;
+import org.apache.cassandra.utils.ByteBufferUtil;
+import org.apache.hadoop.hive.metastore.api.Database;
 import org.apache.thrift.TBase;
+import org.apache.thrift.TDeserializer;
 import org.apache.thrift.TFieldIdEnum;
+import org.apache.thrift.TSerializer;
 import org.apache.thrift.meta_data.FieldMetaData;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,14 +23,67 @@ public class MetaStorePersister
 {
     private static final Logger log = LoggerFactory.getLogger(MetaStorePersister.class);
     
-    public void save(Map<? extends TFieldIdEnum,FieldMetaData> metaData, 
-            TBase<? extends TBase, ? extends TFieldIdEnum> base) 
+    private TSerializer serializer;
+    private TDeserializer deserializer;
+    private Cassandra.Iface client;
+    
+    public MetaStorePersister(Cassandra.Iface client) 
     {
-        for (Map.Entry<? extends TFieldIdEnum,FieldMetaData> entry : metaData.entrySet() )
+        this.client = client;
+    }
+
+    @SuppressWarnings("unchecked")
+    public void save(Map<? extends TFieldIdEnum, FieldMetaData> metaData,
+            TBase base) throws CassandraHiveMetaStoreException
+    {
+        serializer = new TSerializer();
+        Database db = (Database) base;
+        BatchMutation batchMutation = new BatchMutation();
+        if ( log.isDebugEnabled() )
+            log.debug("class: {} dbname: {}", base.getClass().getName(), db.getName());
+        try
         {
-            FieldMetaData fmd = entry.getValue();
-            log.info("FieldMetaData: {}", fmd);
-            //fmd.fieldName
+            batchMutation.addInsertion(ByteBufferUtil.bytes(db.getName()), Arrays.asList("MetaStore"), 
+                    new Column(
+                    ByteBufferUtil.bytes(base.getClass().getName()),
+                    ByteBuffer.wrap(serializer.serialize(base)), System
+                            .currentTimeMillis() * 1000));
+            
+            client.set_keyspace("HiveMetaStore");
+            client.batch_mutate(batchMutation.getMutationMap(),
+                    ConsistencyLevel.QUORUM);
+        } 
+        catch (Exception e)
+        {
+            // TODO add exception handling wrapper
+            throw new CassandraHiveMetaStoreException(e.getMessage(), e);
         }
+
+    }
+    
+    @SuppressWarnings("unchecked")
+    public TBase load(Class clazz, String databaseName) 
+        throws CassandraHiveMetaStoreException
+    {
+        log.debug("class: {} dbname: {}", clazz.getName(), databaseName);
+        deserializer = new TDeserializer();
+        String colName = clazz.getName();
+        TBase base;
+        try 
+        {
+            base = (TBase)clazz.newInstance();
+            client.set_keyspace("HiveMetaStore");
+            ColumnPath columnPath = new ColumnPath("MetaStore");
+            columnPath.setColumn(ByteBufferUtil.bytes(colName));
+            ColumnOrSuperColumn cosc = client.get(ByteBufferUtil.bytes(databaseName), columnPath, ConsistencyLevel.QUORUM);
+            deserializer.deserialize(base, cosc.getColumn().getValue());
+        } 
+        catch (Exception e) 
+        {
+            // TODO same exception handling wrapper as above
+            throw new CassandraHiveMetaStoreException(e.getMessage(), e);
+        }
+                
+        return base;
     }
 }
--- test/unit/org/apache/cassandra/hadoop/hive/metastore/MetaStorePersisterTest.java
+++ test/unit/org/apache/cassandra/hadoop/hive/metastore/MetaStorePersisterTest.java
@@ -1,19 +1,31 @@
 package org.apache.cassandra.hadoop.hive.metastore;
 
+import static org.junit.Assert.*;
+
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
 
 import org.apache.cassandra.CleanupHelper;
 import org.apache.cassandra.EmbeddedServer;
 import org.apache.cassandra.config.ConfigurationException;
+import org.apache.cassandra.config.DatabaseDescriptor;
+import org.apache.cassandra.hadoop.CassandraProxyClient;
+import org.apache.cassandra.thrift.Cassandra;
+import org.apache.cassandra.thrift.CfDef;
+import org.apache.cassandra.thrift.KsDef;
 import org.apache.hadoop.hive.metastore.api.Database;
 import org.apache.thrift.transport.TTransportException;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class MetaStorePersisterTest extends CleanupHelper
 {
 
-    private MetaStorePersister metaStorePersister = new MetaStorePersister();
+    private MetaStorePersister metaStorePersister;
+    private Cassandra.Iface client;
     
     @BeforeClass
     public static void setup() throws TTransportException, IOException, InterruptedException, ConfigurationException
@@ -21,10 +33,47 @@ public class MetaStorePersisterTest extends CleanupHelper
         EmbeddedServer.startBrisk();                
     }
     
+    @Before
+    public void setupClient() throws Exception 
+    {
+        client = CassandraProxyClient.newProxyConnection("localhost",
+                DatabaseDescriptor.getRpcPort(),true,true); 
+        metaStorePersister = new MetaStorePersister(client);
+        
+        CfDef cf = new CfDef("HiveMetaStore", "MetaStore");
+        KsDef ks = new KsDef("HiveMetaStore", "org.apache.cassandra.locator.SimpleStrategy", 1, Arrays.asList(cf));
+        
+        client.system_add_keyspace(ks);        
+    }
+    
     @Test
     public void testBasicPersistMetaStoreEntity() 
     {
         Database database = new Database();
+        database.setName("name");
+        database.setDescription("description");
+        database.setLocationUri("uri");
+        database.setParameters(new HashMap<String, String>());
         metaStorePersister.save(database.metaDataMap, database); // save(TBase base).. via _Fields and findByThriftId, publi MetaDataMap
+    }
+    
+    @Test
+    public void testBasicFindMetaStoreEntity() 
+    {
+        Database database = new Database();
+        database.setName("name");
+        database.setDescription("description");
+        database.setLocationUri("uri");
+        database.setParameters(new HashMap<String, String>());
+        metaStorePersister.save(database.metaDataMap, database);
+        
+        Database foundDb = (Database)metaStorePersister.load(Database.class, "name");
+        assertEquals(database, foundDb);
+    }
+    
+    @After
+    public void teardownClient() throws Exception 
+    {
+        client.system_drop_keyspace("HiveMetaStore");
     }
 }
