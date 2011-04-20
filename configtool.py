695d544584ae1b133f393efb9466dd478a51ec49
Joaquin Casares <joaquin@datastax.com>
2011-04-19 20:10:36 -0500

    configtool.py

--- configtool.py
+++ configtool.py
@@ -67,30 +67,38 @@ def commandLineSwitches():
     parser.add_option("-p", "--hconfPath", action="store", type="string", dest="hconfPath", help="Set hadoop/conf/ path.")
 
     (options, args) = parser.parse_args()
-    print args
+    switchesUsed = False
     if options:
         if options.clusterName:
             clusterName = options.clusterName
+            switchesUsed = True
         if options.seedList:
             seedList = options.seedList
             seedList = seedList.replace(' ', '')
             seedList = seedList.split(',')
             seedList.sort()
+            switchesUsed = True
         if options.clusterSize:
             clusterSize = options.clusterSize
+            switchesUsed = True
         if options.autoBootstrap:
             autoBootstrap = options.autoBootstrap
+            switchesUsed = True
         if options.internalIP:
             internalIP = options.internalIP
+            switchesUsed = True
         if not options.tokenPosition == None:
             tokenPosition = options.tokenPosition
             if not tokenPosition < clusterSize:
                 print "ERROR: Tokens must start at 0 and be less than clusterSize."
                 sys.exit()
+            switchesUsed = True
         if options.confPath:
             confPath = options.confPath
+            switchesUsed = True
         if options.hconfPath:
             hconfPath = options.hconfPath
+            switchesUsed = True
         return True
     else:
         return False
