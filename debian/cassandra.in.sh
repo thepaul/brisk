05fe32417c0bca14f3950df475a001b9e00408af
Brandon Williams <brandon@datastax.com>
2011-04-21 13:18:44 -0500

    Update cassandra.in.sh to point at brisk locations

--- debian/cassandra.in.sh
+++ debian/cassandra.in.sh
@@ -1,10 +1,10 @@
 
 # The directory where Cassandra's configs live (required)
-CASSANDRA_CONF=/etc/cassandra
+CASSANDRA_CONF=/etc/brisk/cassandra
 
 # The java classpath (required)
 CLASSPATH=$CASSANDRA_CONF
 
-for jar in /usr/share/cassandra/*.jar; do
+for jar in /usr/share/brisk/cassandra/*.jar; do
     CLASSPATH=$CLASSPATH:$jar
 done
