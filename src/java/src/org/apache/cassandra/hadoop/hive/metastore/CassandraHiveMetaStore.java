977c901fbef7d9b5a2ac0bc710b4d9c81103537d
T Jake Luciani <jakers@gmail.com>
2011-05-05 00:11:26 -0400

    add ASL headers and remove attributions

--- src/java/src/org/apache/cassandra/hadoop/hive/metastore/CassandraHiveMetaStore.java
+++ src/java/src/org/apache/cassandra/hadoop/hive/metastore/CassandraHiveMetaStore.java
@@ -1,3 +1,20 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.cassandra.hadoop.hive.metastore;
 
 import java.util.*;
@@ -26,8 +43,6 @@ import org.apache.thrift.TBase;
  * Meta information such as roles and privileges (that is, 'entities' that
  * can be cross-database) go into the row with the key '__meta__'
  *
- *
- * @author zznate
  */
 public class CassandraHiveMetaStore implements RawStore {       
     
