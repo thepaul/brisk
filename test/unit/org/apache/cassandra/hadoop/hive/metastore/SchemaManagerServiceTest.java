f3086a96e2c0641f7b39e01d6e99679de30a5b4a
zznate <nate@vervewireless.com>
2011-05-16 14:31:51 -0500

    added schema manager service to encapsulate meta data store creation, new test caes and refactoring of existing test cases for such. partially address BRISK-180

---
+++ test/unit/org/apache/cassandra/hadoop/hive/metastore/SchemaManagerServiceTest.java
@@ -0,0 +1,34 @@
+package org.apache.cassandra.hadoop.hive.metastore;
+
+import static org.junit.Assert.*;
+
+import org.apache.hadoop.conf.Configuration;
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class SchemaManagerServiceTest extends BaseMetaStoreTest
+{
+    private CassandraClientHolder cassandraClientHolder;
+    private Configuration configuration;
+    private SchemaManagerService schemaManagerService;
+    
+    
+    public void setupLocal() throws Exception 
+    {
+        configuration = buildConfiguration();
+        cassandraClientHolder = new CassandraClientHolder(configuration);
+        schemaManagerService = new SchemaManagerService(cassandraClientHolder, configuration);
+    }
+    
+    @Test
+    public void testMetaStoreSchema() throws Exception 
+    {
+        setupLocal();
+        boolean created = schemaManagerService.createMetaStoreIfNeeded();
+        assertTrue(created);
+        created = schemaManagerService.createMetaStoreIfNeeded();
+        assertFalse(created);
+    }
+
+}
